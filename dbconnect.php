c864ba8a8c8978b621f54ea9c67b414613944a00
Atul Kumar Gupta <atul.svnit@yahoo.com>
2016-10-18 02:11:59 +0530

    new heroku cleardb configuration

--- dbconnect.php
+++ dbconnect.php
@@ -1,17 +1,14 @@
-<?php
+<?
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
 $server = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 
-if(!mysql_connect($server,$username,$password))
-{
-     die('oops connection problem ! --> '.mysql_error());
-}
-if(!mysql_select_db($db))
-{
-     die('oops database selection problem ! --> '.mysql_error());
-}
+$config = array(
+    'host' => $server ,
+    'user' => $username ,
+    'pw' => $password,
+    'db' => $db 
+);
 ?>
