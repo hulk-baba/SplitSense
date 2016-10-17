77e0e7a5f43bfdac9c462d47561aef5aeaffec9a
Atul Kumar Gupta <atul.svnit@yahoo.com>
2016-10-18 02:31:54 +0530

    new heroku cleardb configuration

--- dbconnect.php
+++ dbconnect.php
@@ -1,14 +1,17 @@
-<?
+<?php
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
 $server = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 
-$config = array(
-    'host' => $server ,
-    'user' => $username ,
-    'pw' => $password,
-    'db' => $db 
-);
+if(!mysql_connect($server,$username,$password))
+{
+     die('oops connection problem ! --> '.mysql_error());
+}
+if(!mysql_select_db($db))
+{
+     die('oops database selection problem ! --> '.mysql_error());
+}
 ?>
--- index.php
+++ index.php
@@ -1,66 +1,17 @@
 <?php
-session_start();
-include_once 'dbconnect.php';
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-if(isset($_SESSION['user'])!="")
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+if(!mysql_connect($server,$username,$password))
 {
- header("Location: view_txn.php");
+     die('oops connection problem ! --> '.mysql_error());
 }
-if(isset($_POST['btn-login']))
+if(!mysql_select_db($db))
 {
- $email = mysql_real_escape_string($_POST['email']);
- $upass = mysql_real_escape_string($_POST['pass']);
- $res=mysql_query("SELECT * FROM Users WHERE email='$email'");
- $row=mysql_fetch_array($res);
- if($row['password']==md5($upass))
- {
-  $_SESSION['user'] = $row['UserID'];
-  header("Location: view_txn.php");
- }
- else
- {
-  ?>
-        <script>alert('wrong details');</script>
-        <?php
- }
- 
+     die('oops database selection problem ! --> '.mysql_error());
 }
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>SplitSense Login</title>
-<link rel="stylesheet" href="style.css" type="text/css" />
-</head>
-<body>
-<center>
-<div id="login-form">
-<form method="post">
-<table align="center" width="30%" border="0">
-<tr>
-<td><input type="text" name="email" placeholder="Your Email" required /></td>
-</tr>
-<tr>
-<td><input type="password" name="pass" placeholder="Your Password" required /></td>
-</tr>
-<tr>
-<td><button type="submit" name="btn-login">Sign In to SplitSense</button></td>
-</tr>
-<tr>
-<td><a href="register.php">Sign Up To SplitSense</a></td>
-</tr>
-</table>
-</form>
-</div>
-</center>
-</body>
-</html>
